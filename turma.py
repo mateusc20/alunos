e6955a5aed0899c491fa10312d444e02faaab9e5
Hirato001 <124075351+Hirato001@users.noreply.github.com>
2023-01-31 15:04:04 -0300

    Add files via upload
    funçoes cadastrar e listar adc

--- turma.py
+++ turma.py
@@ -1,8 +1,18 @@
-def cadastrar ():
-        print('Cadastrando')
+alunos = []
 
-def listar ():
-        print('Listando')
+def cadastrar():
+    nome = input('Nome: ')
+    turma = input('Turma: ')
+    aluno = [nome, turma]
+    print('Cadastrando...')
+    alunos.append(aluno)
+    print('Cadastro realizado com sucesso')
+
+def listar():
+    print('Listando...')
+    print(f'nome\tturma')
+    for aluno in alunos:
+        print(f'{aluno[0]}\t{aluno[1]}')
 
 while True:
     print('1. Cadastrar Aluno\n2. Listar\n3. Sair')
