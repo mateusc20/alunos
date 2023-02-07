79d713e4d4e1b4bcd0c01b76dd700fe87f30700b
Hirato001 <124075351+Hirato001@users.noreply.github.com>
2023-02-07 15:38:15 -0300

    Add files via upload

--- turma.py
+++ turma.py
@@ -1,21 +1,21 @@
 def cadastrar():
-    print('Cadastrando')
     nome = input('Nome: ')
     turma = input('Turma: ')
-    arquivo = open("alunos.txt", "a")
-    arquivo.write(f"{nome},{turma}\n")
+    print('Cadastrando...')
+    arquivo = open("alunos.txt.", "a")
+    arquivo.write(f"{nome} ,{turma}\n")
     arquivo.close()
-    print('Cadastro realizado')
+    print('Cadastro realizado com sucesso')
 
 def listar():
-    print('Listando')
+    print('Listando...')
     with open("alunos.txt", "r") as arquivo:
-        print('nome\tturma')
         linhas = arquivo.readlines()
-        for linha in linhas:            
-            aluno = linha.strip().split(',')
-            print(f'{aluno[0]}\t{aluno[1]}') 
-              
+    print(f'Nome\tTurma')
+    for linha in linhas:
+        aluno = linha.strip().split(',')
+        print(f'{aluno[0]}\t{aluno[1]}')
+
 while True:
     print('1. Cadastrar Aluno\n2. Listar\n3. Sair')
     opcao = int(input())
@@ -24,5 +24,5 @@ while True:
     elif opcao == 2:
         listar()
     else:
+        print('Até Mais!')
         break
-print('Até Mais!')
